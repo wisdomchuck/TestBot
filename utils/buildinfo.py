a144c05088b6aaa9dcd7d3bea9f885caa19f86be
Dwayne Carter <12144116+wisdomchuck@users.noreply.github.com>
2017-07-24 20:53:22 -0400

    update buildinfo.py
    Updated file based on changes

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
-BUILD_VERSION = "13.0"
+BUILD_VERSION = "13.0.1"
 BUILD_AUTHORS = "Modified by WisdomChuck#7806, orginally by Seth#0051 and based robin#0052"
 BUILD_CODENAME = "Wish we could turn back time... to the good old days"
-BUILD_DATE = "June 2nd, 2017 at 1:25 PM GMT -7:00"
+BUILD_DATE = "July 24th, 2017 at 8:51 PM EDT -4:00"
