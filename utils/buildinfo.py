3955c42d44f3f85bfdb1f30b746ebce1108719a5
Dwayne Carter <12144116+wisdomchuck@users.noreply.github.com>
2017-07-24 21:30:48 -0400

    added S2I information
    added s2i/environment

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
-BUILD_VERSION = "13.0.1"
+BUILD_VERSION = "13.0.2"
 BUILD_AUTHORS = "Modified by WisdomChuck#7806, orginally by Seth#0051 and based robin#0052"
 BUILD_CODENAME = "Wish we could turn back time... to the good old days"
 BUILD_DATE = "July 24th, 2017 at 8:51 PM EDT -4:00"
