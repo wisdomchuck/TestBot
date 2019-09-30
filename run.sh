04fbf9dd83f6cefcf6d4ceb17881ab74ab262681
wisdomchuck <12144116+wisdomchuck@users.noreply.github.com>
2019-09-29 23:50:51 -0400

    updates

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ python -V > /dev/null 2>&1 || {
 	echo >&2 "If you have python 3.5, use it to run bot.py instead of this script."
 	exit 1; }
 
-sed "s/Token =/Token = $token/" config/config.ini > config/config.ini
+sed "s/Token =/Token = $token/" config/config.ini > config/config.ini.new
+mv config.ini.new config.ini
 
 python app.py
